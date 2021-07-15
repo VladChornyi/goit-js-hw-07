156f6e52223ffe54690e5ea67b535cd3146f35f4
Vlad <4ornii.vlad@gmail.com>
2021-07-15 17:34:10 +0300

    выполнены правки 2 задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,10 +6,28 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const ingredList = document.querySelector('ul')
-const addIngridients = ingredients.forEach((item) => {
-  const itemEl = document.createElement('li')
-  itemEl.textContent = item;
-  ingredList.appendChild(itemEl)
-})
- 
+
+
+
+const listEl = document.querySelector('#ingredients');
+
+const ingredList = ingredients.reduce((acc, item) => acc + `<li>${item}</li>`, '');
+
+listEl.innerHTML = ingredList;
+
+
+
+
+
+
+
+// const ingredList = document.querySelector('#ingredients');
+// const addIngridients = function (arr) {
+//   arr.forEach((item) => {
+//     const itemEl = document.createElement('li');
+//     itemEl.textContent = item;
+//     ingredList.appendChild(itemEl);
+//   })
+// };
+// addIngridients(ingredients);
+// console.log(addIngridients(ingredients));

f5306ad686b7c2f788ba4e37c52b6d17ce1837d3
Chornyi Vlad <4ornii.vlad@gmail.com>
2022-04-28 12:14:58 +0300

    add types to task 1

--- js/task-01.tsx
+++ js/task-01.tsx
@@ -1,8 +1,8 @@
-const allCategories = document.querySelectorAll('.item');
+const allCategories:NodeListOf<Element> = document.querySelectorAll('.item');
 console.log(`В документе ${allCategories.length} категории`);
 
 allCategories.forEach(item => {
-  const titleText = item.querySelector('h2').textContent;
-  const quantityItem = item.querySelectorAll('li').length;
+  const titleText: string = item.querySelector('h2').textContent;
+  const quantityItem: number = item.querySelectorAll('li').length;
   console.log(`В категории "${titleText}" количество элементов: ${quantityItem}`);
 });

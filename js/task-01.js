83118279d29d15c58467e631960c96886519352a
Chornyi Vlad <4ornii.vlad@gmail.com>
2022-04-28 12:05:59 +0300

    deleted test file

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,8 @@
-
 const allCategories = document.querySelectorAll('.item');
 console.log(`В документе ${allCategories.length} категории`);
 
-
-
-allCategories.forEach((item) => {
-    const titleText = (item.querySelector('h2').textContent);
-    const quantityItem = (item.querySelectorAll('li').length);
-    console.log(`В категории "${titleText}" количество элементов: ${quantityItem}`);
- });
-
+allCategories.forEach(item => {
+  const titleText = item.querySelector('h2').textContent;
+  const quantityItem = item.querySelectorAll('li').length;
+  console.log(`В категории "${titleText}" количество элементов: ${quantityItem}`);
+});
